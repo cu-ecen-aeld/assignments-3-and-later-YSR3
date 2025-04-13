21977076516e5aeee01ea551b66c9dfcc58fdd87
rmann <rmann@Ubuntu.myguest.virtualbox.org>
2025-04-13 18:09:13 -0500

    Fully removed assignment-1-YSR3 submodule reference

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -20,7 +20,7 @@ bool do_system(const char *cmd)
  * TODO  add your code here
  *  Call the system() function with the command set in the cmd
  *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
+ *   or false() if it returned a failure..
 */
     int sysFunc = system(cmd);
 
