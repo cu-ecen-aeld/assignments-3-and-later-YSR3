c4873571d6e8505aacab44d684a339942ad86b2b
rmann <rmann@Ubuntu.myguest.virtualbox.org>
2025-04-19 09:54:12 -0500

    Assignment 4 Part 1 Commit

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -10,10 +11,27 @@
 
 void* threadfunc(void* thread_param)
 {
-
+    // used: https://man7.org/linux/man-pages/man3/usleep.3.html for 'wait' operation
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    usleep(thread_func_args -> wait_to_obtain_ms);
+
+    int rc = pthread_mutex_lock(thread_func_args -> mutex);
+    if (rc != 0)
+    {
+        printf("pthread_mutex_lock failed with %d\n", rc);
+        thread_func_args -> thread_complete_success = false;
+        return thread_param;
+    }
+
+    usleep(thread_func_args -> wait_to_release_ms);
+
+    pthread_mutex_unlock(thread_func_args -> mutex);
+    thread_func_args -> thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +46,23 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    // allocate memory for the thread called 'data' in a struct containing those listed above.
+    // additionally includes "thread_complete_success" as defined in threading.h struct
+    struct thread_data* data = malloc(sizeof(struct thread_data));
+
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->thread_complete_success = false;
+
+    int rc = pthread_create(thread, NULL, threadfunc, data);
+    if (rc != 0)
+    {
+        return false;
+    }
+
+
+    return true;
 }
 
