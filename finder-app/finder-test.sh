85a1d3bbd3cbcba2f401450ff715deefa3ec3b45
rmann <rmann@Ubuntu.myguest.virtualbox.org>
2025-04-21 23:11:09 -0500

    Adding updated Finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,10 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+
+# Read from Buildroot
+username=$(cat /etc/finder-app/conf/username.txt)
+assignment=$(cat /etc/finder-app/conf/assignment.txt)
 
 if [ $# -lt 3 ]
 then
@@ -54,10 +57,11 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")  #Use system-installed finder.sh
+echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt  #Step 4 Part C
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
